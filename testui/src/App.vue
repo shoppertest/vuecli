db4f21c77258d61d7227fd9c3a0ea6bc2969b816
testshopper <asenshopper@gmail.com>
2019-08-22 14:22:49 +0300

    VUE CLI

---
+++ testui/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin : 0;
+  padding : 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+  }
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
+</style>
---
+++ testui/src/components/AddTodo.vue
@@ -0,0 +1,71 @@
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@shoppertest 
+Learn Git and GitHub without any code!
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+
+ 
+6
+128 61 bradtraversy/vue_crash_todolist
+ Code  Issues 1  Pull requests 0  Projects 0  Wiki  Security  Insights
+vue_crash_todolist/src/components/AddTodo.vue
+@lusarz lusarz Refactor form submit prevent default
+3806a93 on Jan 13
+@bradtraversy @lusarz
+50 lines (42 sloc)  737 Bytes
+    
+<template>
+  <div>
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="Add Todo...">
+      <input type="submit" value="Submit" class="btn">
+    </form>
+  </div>
+</template>
+
+<script>
+import uuid from 'uuid';
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addTodo() {
+      const newTodo = {
+        id: uuid.v4(),
+        title: this.title,
+        completed: false
+      }
+      // Send up to parent
+      this.$emit('add-todo', newTodo);
+      this.title = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+  input[type="submit"] {
+    flex: 2;
+  }
+</style>
+
+
+
